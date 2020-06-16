10b6a55240fa9fdd4c6f91f847e3175928efa6c4
skylarljc <skylarleighjones@gmail.com>
2020-06-16 13:13:39 -0600

    second commit

--- src.cpp
+++ src.cpp
@@ -3,7 +3,8 @@ using namespace std;
 
 int main ()
 {
-  cout << "Hello World!\n ";
+  cout << "Hello World!\n";
   cout << "I'm a C++ program\n";
   cout << "I'm adding this text so I can stage and commit\n";
+  cout << "More text more commit\n";
 }
